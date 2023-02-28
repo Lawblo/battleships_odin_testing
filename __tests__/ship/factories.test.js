07a2c5c9332023aa3c4bf09df9662320173d2990
Thorkil <schjodt.thorkil@gmail.com>
2023-03-01 00:50:52 +0100

    completed placement of ships and board initialization

--- __tests__/ship/factories.test.js
+++ __tests__/ship/factories.test.js
@@ -3,32 +3,62 @@ import { Gameboard } from '../../src/modules/gameboard.js'
 
 describe('test ship', () => {
 
+  const ship = Ship(5)
   it('is not sunk on initialization', () => {
-    const ship = Ship(1)
     expect(ship.is_sunk()).toBe(false)
   })
 
+  test('that ship is created with correct length', () => {
+    expect(ship.length).toBe(5)
+  })
+
   test('that ship is sunk if hits == length', () => {
-    const ship = Ship(1)
+    ship.hit()
+    ship.hit()
+    ship.hit()
+    ship.hit()
+    expect(ship.is_sunk()).toBe(false)
     ship.hit()
     expect(ship.is_sunk()).toBe(true)
   })
 
-  test('that ship is created with correct length', () => {
-    const ship = Ship(5)
-    expect(ship.length).toBe(5)
-  })
 })
 
 
 describe('test gameboard', () => {
-  test('place ships with the ship factory', () => {
+  test('place ship puts ship in ships', () => {
     const gameboard = Gameboard()
-
+    gameboard.place_ship(3, [0, 0])
+    expect(gameboard.ships.length).toBe(1)
+    expect(gameboard.ships[0].length).toBe(3)
   })
-  test('gather missed attacks', () => { })
-  test('gather successful attacks on correct ship', () => { })
-  test('correct placement of ship', () => { })
-  test('wether all ships of player has been sunk', () => { })
-  test('correct placement of ship', () => { })
+
+  test('adding 3 length h ship at 0.0, 0.1, 0.2', () => {
+    const gameboard = Gameboard()
+    gameboard.place_ship(3, [0, 0], true)
+    const ship = gameboard.ships[0]
+    expect(gameboard.board_content[0][0]).toBe(ship)
+    expect(gameboard.board_content[0][1]).toBe(ship)
+    expect(gameboard.board_content[0][2]).toBe(ship)
+  })
+  test('adding 3 length v ship at 0.0, 0.1, 0.2', () => {
+    const gameboard = Gameboard()
+    gameboard.place_ship(3, [0, 0], false)
+    const ship = gameboard.ships[0]
+    expect(gameboard.board_content[0][0]).toBe(ship)
+    expect(gameboard.board_content[1][0]).toBe(ship)
+    expect(gameboard.board_content[2][0]).toBe(ship)
+  })
+
+  test('return false if ship is placed on occupied spot', () => {
+    const gameboard = Gameboard()
+    expect(gameboard.place_ship(3, [0, 0], true)).toBe(true)
+    expect(gameboard.place_ship(2, [1, 0], true)).toBe(false)
+  })
+
+  test('ship is not allowed to be placed off the board', () => {
+    const gameboard = Gameboard()
+    expect(gameboard.place_ship(3, [6, 6], true)).toBe(false)
+    expect(gameboard.place_ship(3, [0, 0], true)).toBe(true)
+  })
 })
--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -8,11 +8,69 @@
 import { Ship } from './ship.js'
 
 function Gameboard() {
+  let board_content = init_board(7)
   let missed_attacks = []
   let ships = []
   let destroyed_ships = []
 
-  function place_ship() {}
+  function init_board(size) {
+    let board = new Array(size).
+      fill(
+        new Array(size).fill([])
+      )
+    return board
+  }
+
+  function place_ship(length, start, is_horizontal = true) {
+    let ship = Ship(length)
+    let place_coords = is_horizontal
+      ? get_horizontal(length, start)
+      : get_vertical(length, start)
+
+    if (place_coords.some(ship_off_board) || place_coords.some(ship_overlaps)) {
+      return false
+    }
+
+    place_coords.forEach(coord => add_to_board(coord, ship))
+    ships.push(ship)
+    return true
+  }
+
+  function add_to_board(coord, ship) {
+    board_content[coord[1]][coord[0]] = ship
+  }
+
+  function get_horizontal(length, start) {
+    let coords = []
+    for (let x = start[1]; x < start[1] + length; x++) {
+      let y = start[0]
+      coords.push([x, y])
+    }
+    return coords
+  }
+
+  function get_vertical(length, start) {
+    let coords = []
+    for (let y = start[0]; y < start[0] + length; y++) {
+      let x = start[1]
+      coords.push([x, y])
+    }
+    return coords
+  }
+  function ship_off_board(coordinates) {
+    return coordinates.some(coord => coord > 6 || coord < 0)
+  }
+
+
+  function ship_overlaps(coordinates) {
+    const x = coordinates[1]
+    const y = coordinates[0]
+
+    if (board_content[y][x].length !== 0) {
+      return true
+    }
+    return false
+  }
 
   function receive_attack(coordinates) {
     //determine whether the attack hit
@@ -27,7 +85,7 @@ function Gameboard() {
     return false
   }
 
-  return {place_ship, all_ships_sunk, missed_attacks}
+  return { ships, board_content, place_ship, all_ships_sunk, missed_attacks }
 }
 
 export { Gameboard }
